9a5eb64988d756368ceed1bba77efe4e4677694b
lin-mohan <2419588726@qq.com>
2022-04-18 20:48:22 +0800

    mouse

--- js/mouse.js
+++ js/mouse.js
@@ -1,25 +1,25 @@
+let menu_ul = document.getElementById('menu_ul');
+let audio = document.getElementById('audio');
+
+$("#menu_ul, #audio").unbind("mouseenter").mouseenter(function(){
+    mouse.setAttribute('style','width: 25px;height: 25px;background: rgba(255,255,255,0.2);border: 1px solid rgba(255, 255, 255, 0.7)');
+})
+$("#menu_ul, #audio").unbind("mouseleave").mouseleave(function(){
+    mouse.setAttribute('style','width: 30px;height: 30px;background: rgba(255,255,255,0);border: 1px solid rgba(0, 0, 0, 0.7)');
+})
+
 let mouse = document.getElementById('mouse');
-// let scrollY = document.documentElement.scrollTop || document.body.scrollTop;
-// console.log(scrollY);
-let ex = 0,ey = 0,vx = 0,vy = 0,count = 0;
+let ex = 0,ey = 0,vx = 0,vy = 0;
 window.addEventListener('mousemove',function (evt) {    //添加句柄，当鼠标点击时，触发函数
-    ex = evt.x - mouse.offsetLeft - mouse.clientWidth/2;
-    ey = evt.y - mouse.offsetTop - mouse.clientHeight/2;
-    count = 0;
+    ex = evt.x - mouse.clientWidth/2;
+    ey = evt.y - mouse.clientHeight/2;
+    ex +=0.2;
+    ey +=0.2;
 });
 function draw(){
-    if(count < 10){
-        vx += ex/10;
-        vy += ey/10;
-    }
-    mouse.style.left = vx + 'px';
-    mouse.style.top = vy + 'px';
-    count++;
+    mouse.style.left = ex + 'px';
+    mouse.style.top = ey + 'px';
+}  
+setInterval(draw,20)
 
-}
-    
-setInterval(draw,10)
-// menu_ul.addEventListener('mousemove',function (tr){
-//     mouse.style.width = '10PX';
-//     mouse.style.height = '10PX';
-// })
+
